4c55fe179f821394e0193915d6d9efe6852e086d
James Johnston <james@ezmuze.co.uk>
2023-08-01 18:04:31 +0100

    made more reliable ... by relying on the esp32 to crash in bad situations!

--- APS300.ino
+++ APS300.ino
@@ -18,27 +18,13 @@ byte mac[] = {0x00, 0x10, 0xFA, 0x6F, 0x38, 0x4A};
 WiFiClient client;
 HADevice hadevice(mac, sizeof(mac));
 HAMqtt mqtt(client, hadevice);
-unsigned long currentTime = millis();
-unsigned long previousTime = 0;
-const long timeoutTime = 2000;
 
-static boolean doConnect = false;
-static boolean connected = false;
-static boolean doScan = false;
-int scanTime = 5; //In seconds
-bool keepScanning = true;
 BLEAdvertisedDevice device;
 BLEScan* pBLEScan;
+static BLEAdvertisedDevice* myDevice;
 static BLEAddress *pServerAddress;
 static BLEUUID serviceUUID("0000fff0-0000-1000-8000-00805f9b34fb");
 static BLEUUID    charUUID("0000fff1-0000-1000-8000-00805f9b34fb");
-static bool haveAnnounced = false;
-static int nonAnnounceCount = 0;
-
-// Client variables
-char linebuf[80];
-int charcount = 0;
-
 static BLERemoteCharacteristic* pRemoteCharacteristic;
 
 HASensorNumber outputPower("outputPower", HASensorNumber::PrecisionP1);
@@ -49,6 +35,56 @@ HASensorNumber batteryLevel("batteryLevelPercentage", HASensorNumber::PrecisionP
 HABinarySensor acsensor("acOn");
 HABinarySensor dcsensor("dcOn");
 HABinarySensor ledsensor("ledOn");
+
+static void notifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
+  byte b = (byte)pData[7];
+  acsensor.setState(bitRead(b, 1) == 1);
+  dcsensor.setState(bitRead(b, 0) == 1);
+  ledsensor.setState(bitRead(b, 4) == 1);
+
+  outputPower.setValue((256 * pData[11]) + pData[12]);
+  inputPower.setValue((pData[9] * 256) + pData[10]);
+
+  batteryLevel.setValue(pData[8]);
+  minutesRemaining.setValue((256 * pData[13]) + pData[14]);
+  Serial.println("Updating HA");
+}
+
+
+class MyClientCallback : public BLEClientCallbacks {
+    void onConnect(BLEClient* pclient) {
+    }
+
+    void onDisconnect(BLEClient* pclient) {
+      Serial.println("onDisconnect");
+    }
+};
+
+BLEClient* connectToServer(BLEAdvertisedDevice* device) {
+  BLEClient* pClient = BLEDevice::createClient();
+  pClient->setClientCallbacks(new MyClientCallback());
+  if (pClient->connect(device)) { // Connect to the remote BLE Server.
+    BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+    if (pRemoteService == nullptr) {
+      pClient->disconnect();
+    }
+
+    // Obtain a reference to the characteristic in the service of the remote BLE server.
+    pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+    if (pRemoteCharacteristic == nullptr) {
+      pClient->disconnect();
+    }
+
+    if (pRemoteCharacteristic->canNotify())
+      pRemoteCharacteristic->registerForNotify(notifyCallback);
+
+    return pClient;
+  }
+  else {
+    return NULL;
+  }
+}
+
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
       Serial.print("Found ");
@@ -57,12 +93,14 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
         advertisedDevice.getScan()->stop(); //Scan can be stopped, we found what we are looking for
         pServerAddress = new BLEAddress(advertisedDevice.getAddress());
         Serial.println("Found correct device...");
-        doConnect = true;
+
+        Serial.println("Attempting connect");
+        myDevice = new BLEAdvertisedDevice(advertisedDevice);
+       
       }
     }
 };
 
-static bool connectToWifi = false;
 void initWiFi(String ssid, char* password) {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -72,7 +110,6 @@ void initWiFi(String ssid, char* password) {
     delay(1000);
   }
   Serial.println(WiFi.localIP());
-  connectToWifi = true;
 
   Serial.println("Starting MQTT");
   startMqtt();
@@ -97,16 +134,6 @@ void scanWiFi() {
     Serial.print(n);
     Serial.println(" networks found");
     for (int i = 0; i < n; ++i) {
-      // Print SSID and RSSI for each network found
-      Serial.print(i + 1);
-      Serial.print(": ");
-      Serial.print(WiFi.SSID(i));
-      Serial.print(" (");
-      Serial.print(WiFi.RSSI(i));
-      Serial.print(")");
-      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*");
-
-
       if (WiFi.SSID(i) == "PatMorita") {
         Serial.println("Looks like a great network!");
         initWiFi(WiFi.SSID(i), "rubberduck");
@@ -127,6 +154,8 @@ void scanWiFi() {
 
 void setup() {
   Serial.begin(115200);
+  scanWiFi();
+  triggerBLEScan();
 }
 
 void triggerBLEScan() {
@@ -138,11 +167,13 @@ void triggerBLEScan() {
   pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
   pBLEScan->setInterval(100);
   pBLEScan->setWindow(99);  // less or equal setInterval value
+
+  BLEScanResults foundDevices = pBLEScan->start(5, false);
 }
 
 void startMqtt() {
   hadevice.setName("AllPowerss S300");
-  hadevice.setSoftwareVersion("1.0.1");
+  hadevice.setSoftwareVersion("1.0.2");
 
   outputPower.setIcon("mdi:transmission-tower-export");
   outputPower.setName("Output Wattage");
@@ -176,135 +207,74 @@ void startMqtt() {
 }
 
 
-static void notifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify) {
-  byte b = (byte)pData[7];
-  acsensor.setState(bitRead(b, 1) == 1);
-  dcsensor.setState(bitRead(b, 0) == 1);
-  ledsensor.setState(bitRead(b, 4) == 1);
-
-  outputPower.setValue((256 * pData[11]) + pData[12]);
-  inputPower.setValue((pData[9] * 256) + pData[10]);
-
-  batteryLevel.setValue(pData[8]);
-  minutesRemaining.setValue((256 * pData[13]) + pData[14]);
-  haveAnnounced = true;
-  nonAnnounceCount = 0;
-  Serial.println("Announced");
-}
-
-
-class MyClientCallback : public BLEClientCallbacks {
-    void onConnect(BLEClient* pclient) {
-    }
-
-    void onDisconnect(BLEClient* pclient) {
-      connected = false;
-      Serial.println("onDisconnect");
-    }
-};
-
-BLEClient* connectToServer(BLEAdvertisedDevice* device) {
-  BLEClient* pClient = BLEDevice::createClient();
-  pClient->setClientCallbacks(new MyClientCallback());
-  if (pClient->connect(device)) { // Connect to the remote BLE Server.
-    BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
-    if (pRemoteService == nullptr) {
-      pClient->disconnect();
-    }
-
-    // Obtain a reference to the characteristic in the service of the remote BLE server.
-    pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
-    if (pRemoteCharacteristic == nullptr) {
-      pClient->disconnect();
-    }
-    connected = true;
-
-    if (pRemoteCharacteristic->canNotify())
-      pRemoteCharacteristic->registerForNotify(notifyCallback);
-
-    return pClient;
-  }
-  else {
-    return NULL;
-  }
-}
-
-bool foundBattery = false;
 void scanForBattery() {
   Serial.println("Scanning BLE Results for battery");
-  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  BLEScanResults foundDevices = pBLEScan->start(5, false);
   for (int i = 0; i < foundDevices.getCount(); i++) {
     device = foundDevices.getDevice(i);
 
     String deviceName = device.getName().c_str();
     if (deviceName == "AP S300 V2.0") {
       BLEAdvertisedDevice* myDevice = new BLEAdvertisedDevice(device);
-      keepScanning = false;
       delay(2000);
       connectToServer(myDevice);
       BLEClient*  pClient  = BLEDevice::createClient();
       pClient->setClientCallbacks(new MyClientCallback());
       pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
       Serial.println(" - Connected to server");
-      foundBattery = true;
     }
   }
 }
 
-int batteryScan = 0;
-int haveTriggeredBLEScan = 0;
+bool doneConnect = false;
 void loop() {
+  mqtt.loop();
 
-  if (!connectToWifi) {
-    scanWiFi();
-    delay(1000);
+  if (!doneConnect) {
+     delay(2000);
+
+        Serial.println("Connecting to server");
+
+
+        BLEClient* pClient = BLEDevice::createClient();
+        Serial.println("client created");
+        pClient->setClientCallbacks(new MyClientCallback());
+        Serial.println("callbacks set up");
+        delay(1000);
+        Serial.println("delayed");
+        if (pClient->connect(myDevice)) { // Connect to the remote BLE Server.
+          Serial.println("connectified");
+          BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+          if (pRemoteService == nullptr) {
+            Serial.println("remote service is null");
+            pClient->disconnect();
+          }
+
+          // Obtain a reference to the characteristic in the service of the remote BLE server.
+          pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+          if (pRemoteCharacteristic == nullptr) {
+            Serial.println("remote char is null");
+            pClient->disconnect();
+          }
+
+          if (pRemoteCharacteristic->canNotify()) {
+
+            Serial.println("registered for notify");
+            pRemoteCharacteristic->registerForNotify(notifyCallback);
+            doneConnect = true;
+          }
+
+        } else {
+          Serial.println("Failed to connect");
+        }
+
+
+        Serial.println("Connected");
+
+//        pClient->setClientCallbacks(new MyClientCallback());
+//        Serial.println("callbacks setup");
+//        pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+//        Serial.println(" - Connected to server");
   }
-
-  if (connectToWifi) {
-    mqtt.loop();
-
-  }
-
-  if (!doConnect) {
-    Serial.println("Still waiting to find battery");
-    if (haveTriggeredBLEScan == 0) {
-      haveTriggeredBLEScan = 100;
-      triggerBLEScan();
-    } else {
-      Serial.print("Waiting to retrigger BLE Scan: ");
-      Serial.println(haveTriggeredBLEScan);
-      haveTriggeredBLEScan--;
-
-      scanForBattery();
-
-    }
-  } else {
-
-    if (!foundBattery) {
-      if (batteryScan == 0) {
-        batteryScan = 50;
-        scanForBattery();
-      } else {
-        batteryScan-- ;
-        Serial.print("BLE Scan count down...");
-        Serial.println(batteryScan);
-      }
-    }
-  }
-
-  if (foundBattery) {
-    nonAnnounceCount++;
-    if (!haveAnnounced) {
-      Serial.println("Waiting to announce");
-
-      if (nonAnnounceCount > 30) {
-
-        Serial.println("Rebooting sue to non announce");
-        ESP.restart();
-      }
-    }
-
-  }
-
   delay(1000);
 }
